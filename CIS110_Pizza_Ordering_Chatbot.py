7a440f95ff9aa26e08a00ff948288a0de35962cb
username <useremail>
2023-11-05 11:27:49 -0800

    Added Data Validation While loops.

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,10 +1,12 @@
 #Greeting
-print("Hello, my name is ALex your virtual assistant. I will help you order a pizza!")
+print("Hello, my name is Alex your virtual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press enter.")
 
 #Ask user to input their name and store it in a variable
 userName = input("\nEnter your name: ")
-
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name:  ")
+    
 #Output a welcome sentence using the name stored in the variable
 if userName.lower() == "david bragg":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
@@ -13,10 +15,20 @@ else:
 
 #Inputs needed from the user
 size = input("\nWhat size would you like? Enter small, medium, or large:  ")
+while size.lower() not in ["small", "medium", "large" ]:
+    size = input("Invalid vaule!  Please enter small, medium, or large:  ")
+    
 flavor = input("\nWhat kind of flavor would you like?:  ")
+while len(flavor) == 0:
+    flavor = input("Flavor cannot be blank!  Please enter a flavor:  ")
+    
 crustType = input("\nWhat type of crust would you like?:  ")
+while len(crustType) == 0:
+    crustType = input("Crust type cannot be blank!  Please enter crust type:  ")
+    
 quantity = input("\nHow many of those would you like to order? Enter a numeric value:  ")
-#Convert the characters the user typed in a interger
+while not quantity.isdigit():
+    quanitiy = input("Vaule not recognized. Please enter a numeric vaule:  ")
 quantity = int(quantity)
 
 method = input("\nIs this for delivery or carry out?:  ")
